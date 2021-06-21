dee8289a28bcd2cf30a5163b997a57b15a1746e8
Spm-Eng <dlspm0512@gmail.com>
2021-06-21 19:29:54 +0800

    feat: JS scrollTo 釘選下選單 + nav tab

--- js/all.js
+++ js/all.js
@@ -4,26 +4,34 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
-// mobile btn toggle
-const bottomBtn = document.getElementById("bottomBtn");
-const pageFooter = document.querySelector('#pageFooter');
-const productTab = document.querySelector('#productTab');
-
-let productTabHeight = productTab.offsetTop + productTab.offsetHeight;
-// mobile 置底按鈕出現時機
-const bottomBtnScroll = () => {
-  // 不同裝置取得方式
-  const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
-  if (scrollTop < productTabHeight || scrollTop > pageFooter.offsetTop - window.screen.height) {
-    bottomBtn.classList.add('d-none');
-  } else {
-    bottomBtn.classList.remove('d-none');
-  }
-}
-
-const goToScroll = scrollNumber => {
+// click nav&tab go there 
+function goToTab() {
   window.scrollTo({
-    top: scrollNumber,
-    behavior: 'smooth'
-  })
+    top: 1010,
+    behavior: "smooth",
+  });
 }
+// click go to form
+function goToForm() { 
+  // 4125 代表 form 置頂時滾動軸的高度 - 150 預留空白高度
+  window.scrollTo({
+    top: 4125 - 150,
+    behavior: "smooth",
+  });
+}
+
+// mobile btn toggle 下選單邏輯：先隱藏 -> nat tab sticky 後再顯示
+// 位置高度：navIsSticky 代表 tab 已經 sticky top，scrollIsBottom 代表滾到最底部（190 高度 = 專案按鈕 + footer）
+var bottomBtn = document.getElementById("bottomBtn");
+window.onscroll = function () {
+  const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  const navIsSticky = window.pageYOffset > 1000
+  const scrollIsBottom = scrollTop + document.documentElement.clientHeight < document.body.scrollHeight - 190
+  if (bottomBtn !== null && document.body.offsetWidth < 768) {
+    if(navIsSticky && scrollIsBottom) {
+      bottomBtn.classList.remove('d-none');
+    }else{
+      bottomBtn.classList.add('d-none');
+    }
+  }
+};
